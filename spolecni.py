b56a2212c9419c2150427bfe333ec9f78c285bbe
DimaLOp28 <sofiia.nehreba@infis.cz>
2025-04-01 10:56:30 +0200

    Update spolecni.py

--- spolecni.py
+++ spolecni.py
@@ -1,30 +1,34 @@
-#Diana B
-aktivita1 = input("Jakou aktivitu jste dělali? ") 
-aktivita2 = input("Jakou druhou aktivitu jste dělali? ") 
-delka1 = int(input("Jaká je délka první aktivity? V minutách: ")) 
-delka2 = int(input("Jaká je délka druhé aktivity? V minutách: "))
+#Ukol A(Sofiia)
+  
+jidlo1 = input("Zadej první jídlo: ")
+kalorie1 = int(input(f"Kolik kalorií má {jidlo1}? "))
  
-kcal1 = delka1 * 5 
-kcal2 = delka2 * 5 
-celkove_spalene = kcal1 + kcal2
+jidlo2 = input("Zadej druhé jídlo: ")
+kalorie2 = int(input(f"Kolik kalorií má {jidlo2}? "))
  
- #Sofiia A
-jidlo1 = input("Zadej první jídlo: ") 
-kalorie1 = int(input(f"Kolik kalorií má {jidlo1}? ")) 
-jidlo2 = input("Zadej druhé jídlo: ") 
-kalorie2 = int(input(f"Kolik kalorií má {jidlo2}? ")) 
-jidlo3 = input("Zadej třetí jídlo: ") 
+jidlo3 = input("Zadej třetí jídlo: ")
 kalorie3 = int(input(f"Kolik kalorií má {jidlo3}? "))
  
- 
-celkove_kalorie = kalorie1 + kalorie2 + kalorie3
- 
- 
-bilance = celkove_kalorie - celkove_spalene
+celkoveKalorie = kalorie1 + kalorie2 + kalorie3
+print(f"Celkový počet kalorií za den: {celkoveKalorie}")
  
  
-print(f"Snedl/a jsi {celkove_kalorie:.1f} kcal a spálil/a {celkove_spalene:.1f} kcal.") 
-print(f"Bilance: {bilance:.1f} kcal") 
-if bilance > 0: print(f"Přebýtek: {bilance:.1f} kcal") 
-elif bilance < 0: print(f"Nedostatek: {-bilance:.1f} kcal") 
-else: print("Přesná rovnováha kalorií.")
+#Ukol B(Diana)
+aktivita1=str(input("Jakou aktivitu jste dělali? "))
+aktivita2=str(input("Jakou druhou aktivitu jste dělali? "))
+delka1=int(input("Jaká je délka první aktivity? V minutách: "))
+delka2=int(input("Jaká je délka druhý aktivity? V minutách: "))
+ 
+kcal1=delka1*5
+kcal2=delka2*5
+print("Celkový počet kalorie je: ", kcal1+kcal2)
+ 
+#Společný
+snedl=celkoveKalorie
+spalil=kcal1+kcal2
+if(snedl-spalil>0):
+    print("Máte přebytek kalorie")
+elif(snedl-spalil<0):
+    print("Máte nedostatek kalorie")
+else:
+    print()
